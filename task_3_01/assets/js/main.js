5a63d89bb5ef6450b2f5daa5c782cd83bef7c853
AleXxxWEB <alexivakha@gmail.com>
2022-04-16 23:10:40 +0300

    vue validation

--- task_3_01/assets/js/main.js
+++ task_3_01/assets/js/main.js
@@ -1,38 +1,66 @@
-Vue.use(vuelidate.default);
-
-new Vue({
-    el: "#app",
-    data: {
-        firstName: "",
-        lastName: "",
-        gender: "",
-        bg: '',
-    },
-    validations: {
-        firstName: {
-            required: validators.required,
-            minLength: validators.minLength(4)
-        },
-        lastName: {
-            required: validators.required,
-            minLength: validators.minLength(4)
-        },
-        gender: {},
+let App = {
+    data() {
+        return {
+            firstName: '',
+            lastName: '',
+            checkGender: '',
+            phone: '',
+            phoneRegex: '^(?:\\+38)?(0\\d{9})$',
+            nickName: '',
+            nickNameRegex: '^[a-zA-Z0-9_]*$',
+            bgColor: '',
+        }
     },
     methods: {
-        submit: function () {
-            if (this.$v.$invalid) {
-                return;
-            }
-            alert("Thank you for submitting!");
-            this.$v.$reset();
-        },
-        genderWatch: function() {
-            if (this.value == 'male') {
-                this.bg = 'yellow';
+        checkFirstName() {
+            let firstName =  document.querySelector('[name="firstName"]');
+
+            if (!this.firstName) {
+                firstName.style.border = "2px solid red";
             } else {
-                this.bg = 'pink';
+                firstName.style.border = "";
             }
         },
-    }
-});
+
+        checkLastName() {
+            let lastName =  document.querySelector('[name="lastName"]');
+
+            if (!this.lastName) {
+                lastName.style.border = "2px solid red";
+            } else {
+                lastName.style.border = "";
+            }
+        },
+
+        checkPhone() {
+            let phone =  document.querySelector('[name="phone"]');
+
+            if (!this.phone.match(this.phoneRegex)) {
+                phone.style.border = "2px solid red";
+            } else {
+                phone.style.border = "";
+            }
+        },
+
+        checkNickName() {
+            let nickName =  document.querySelector('[name="nickName"]');
+
+            if (!this.nickName.match(this.nickNameRegex)) {
+                nickName.style.border = "2px solid red";
+            } else {
+                nickName.style.border = "";
+            }
+        }
+    },
+    watch: {
+        checkGender(val) {
+            if (val == 'female') {
+                this.bgColor = 'lightcoral';
+            } else {
+                this.bgColor = 'dodgerblue';
+            }
+        }
+    },
+}
+
+Vue.createApp(App).mount('#app');
